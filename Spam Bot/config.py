9e0f50c9b4a61e3d5268758fd0e6008e48cf403b
Rouinz <136705321+Rouinz@users.noreply.github.com>
2023-11-13 20:17:04 +0700

    Add files via upload

--- Spam Bot/config.py
+++ Spam Bot/config.py
@@ -1,4 +1,4 @@
 counter = 1
-token = 'ODYxNDQ0MjY1NzQxNzc4OTk0.GtmxQ3.EB8LC2P3dMBMNHa-9IANCQYoOXYTLuqYcLsEXQ'
-channel_link = 'https://discord.com/api/v9/channels/1173261609196605490/messages'
+token = ''
+channel_link = ''
 rand = False
